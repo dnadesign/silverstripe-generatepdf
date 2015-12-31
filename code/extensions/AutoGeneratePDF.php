1b2f08128a20b2c62d5cd87ff248b1f69ec60459
helpfulrobot <chris@silverstripe.com>
2015-12-31 23:52:17 +1300

    Converted to PSR-2

--- code/extensions/AutoGeneratePDF.php
+++ code/extensions/AutoGeneratePDF.php
@@ -4,20 +4,19 @@
 * Generates a PDF version of the page upon publish
 */
 
-class AutoGeneratePDF extends DataExtension {	
+class AutoGeneratePDF extends DataExtension
+{
 
-	/**
-	 * Remove linked pdf when publishing the page,
-	 * as it would be out of date.
-	 * And generate new copy
-	 */
-	public function onAfterPublish(&$original) {
-		if ($this->owner->hasExtension('GeneratePDF')) {
-			// Generates new PDF
-			$this->owner->doGeneratePDF();
-		}
-	}	
-
+    /**
+     * Remove linked pdf when publishing the page,
+     * as it would be out of date.
+     * And generate new copy
+     */
+    public function onAfterPublish(&$original)
+    {
+        if ($this->owner->hasExtension('GeneratePDF')) {
+            // Generates new PDF
+            $this->owner->doGeneratePDF();
+        }
+    }
 }
-
-
--- code/tasks/CleanupGeneratedPDF.php
+++ code/tasks/CleanupGeneratedPDF.php
@@ -1,33 +1,39 @@
 <?php
-class CleanupGeneratedPDF extends BuildTask {
+class CleanupGeneratedPDF extends BuildTask
+{
 
-	protected $title = 'Cleanup generated PDFs ';
-	
-	protected $description = 'Removes generated PDFs on the site, forcing a regeneration of all exports to PDF when users
+    protected $title = 'Cleanup generated PDFs ';
+    
+    protected $description = 'Removes generated PDFs on the site, forcing a regeneration of all exports to PDF when users
 		go to download them. This is most useful when templates have been changed so users should receive a new copy';
 
-	public function run($request) {
-		$path = sprintf('%s/%s', BASE_PATH, GeneratePDF::getPath());
-		if(!file_exists($path)) return false;
+    public function run($request)
+    {
+        $path = sprintf('%s/%s', BASE_PATH, GeneratePDF::getPath());
+        if (!file_exists($path)) {
+            return false;
+        }
 
-		$files = scandir($path);
-		if (count($files) - 2 === 0) return false;
+        $files = scandir($path);
+        if (count($files) - 2 === 0) {
+            return false;
+        }
 
-		exec(sprintf('rm %s/*', $path), $output, $return_val);
+        exec(sprintf('rm %s/*', $path), $output, $return_val);
 
-		// output any errors
-		if($return_val != 0) {
-			user_error(sprintf('%s failed: ', get_class($this)) . implode("\n", $output), E_USER_ERROR);
-		}
-	}
+        // output any errors
+        if ($return_val != 0) {
+            user_error(sprintf('%s failed: ', get_class($this)) . implode("\n", $output), E_USER_ERROR);
+        }
+    }
 }
 
-class CleanupGeneratePagedPdfDailyTask extends DailyTask {
+class CleanupGeneratePagedPdfDailyTask extends DailyTask
+{
 
-	public function process() {
-		$task = new CleanupGeneratedPDF();
-		$task->run(null);
-	}
-
+    public function process()
+    {
+        $task = new CleanupGeneratedPDF();
+        $task->run(null);
+    }
 }
-
